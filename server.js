34d89d3413119b3b012688caa93504f8e1d52611
Erneris <ernestassin1@gmail.com>
2019-04-25 11:23:03 +0000

    updates

--- server.js
+++ server.js
@@ -3,10 +3,10 @@ var express = require('express');
 var app = express();
 var path = require('path');
 
-app.use(express.static(path.join(__dirname, 'knygos')));
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname + '/knygos/index.html'));
+  res.sendFile(path.join(__dirname + '/public/index.html'));
 });
 
 app.listen(8080, function () {
