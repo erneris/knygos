692a909b067d56c699eee6c0cc059f55203d8543
Erneris <ernestassin1@gmail.com>
2019-04-25 13:45:07 +0000

    SQl

--- server.js
+++ server.js
@@ -1,7 +1,15 @@
-
 var express = require('express');
 var app = express();
 var path = require('path');
+var mysql = require('mysql');
+
+
+var con = mysql.createConnection({
+  hostname: "erneris-html-6500297",
+  user: "erneris",
+  password: "",
+  database: "c9"
+});
 
 app.use(express.static(path.join(__dirname, 'public')));
 
@@ -10,6 +18,18 @@ app.get('/', function (req, res) {
 });
 
 app.listen(8080, function () {
-  console.log('Example app listening on port 8080!');
-  //https://html-erneris.c9users.io
+  console.log('Puslapis pasileido https://html-erneris.c9users.io!');
+});
+app.get('/uzregistruoti', (req, res) => {
+
+
+con.connect(function(err) {
+  if (err) throw err;
+  var sql = "INSERT INTO emails(email) VALUE ('"+req.body.Email+"')";
+  con.query(sql, function (err, result) {
+    if (err) throw err;
+    console.log("1 record inserted, ID: " + result.insertId);
+  });
+});
+
 });
